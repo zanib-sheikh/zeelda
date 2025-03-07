2a01eb60eefc5d26455deb225f6e3c491bc757a2
Zanib Zubair <166975122+zanib-sheikh@users.noreply.github.com>
2025-03-08 00:14:19 +0500

    Update TV.jsx

--- src/components/TV.jsx
+++ src/components/TV.jsx
@@ -80,9 +80,9 @@ function TV() {
 
 
 
-  <div className="flex items-start bg-white w-full gap-6 py-6 px-14  mx-auto">
+  <div className="flex items-center justify-center bg-white w-full gap-6 py-6 px-14  mx-auto">
       {/* Image Section (1/3 Width) */}
-      <div className="relative w-[822px] h-[500px] mb-11">
+      <div className="relative w-[922px] h-[500px] mb-11">
         <img
           src={tv}
           alt="Thumbnail"
@@ -97,7 +97,7 @@ function TV() {
       </div>
 
       {/* Comments Section (2/3 Width) */}
-      <div className="w-[560px] h-[400px] flex flex-col  justify-between">
+      <div className="w-1/3 flex flex-col  justify-between">
        
       <div className="flex justify-between  border-b  px-14">
       <h2
